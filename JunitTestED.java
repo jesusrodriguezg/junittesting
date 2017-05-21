38b459a9328e19d2cca27c2581e6090dbfbbd898
Jesús Rodríguez <sanedu@gmail.com>
2017-05-21 19:52:05 +0200

    Revert "Maven"
    This reverts commit 88d2ce15615c306b20dbc614cb48eeb563fdfdf4.

---
+++ JunitTestED.java
@@ -0,0 +1,10 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   MathTest.class,
+   TestPerson.class
+})
+public class JunitTestED {   
+}  	
